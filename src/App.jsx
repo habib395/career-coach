038eb7fea736c2ebd7f2eff604d28a7517249574
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-11-17 19:45:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Career Counseling Website</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5UIQp_mnH_hrNWfTy5RKw2RIe8XpkXkc",
+  authDomain: "career-counseling-websit-df0bb.firebaseapp.com",
+  projectId: "career-counseling-websit-df0bb",
+  storageBucket: "career-counseling-websit-df0bb.firebasestorage.app",
+  messagingSenderId: "949493492736",
+  appId: "1:949493492736:web:40670b26cc1f7b6e983ce5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
