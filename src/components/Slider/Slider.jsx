13e96df61867262a866f8644ef46971095791ea1
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-11-17 21:43:04 +0600

    slider section added

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Slider = () => {
+  return (
+    <div className="carousel w-full h-96  mx-auto">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="../../../public/img-one.png"
+          className="w-full"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+          src="../../../public/img-two.png"
+          className="w-full"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img
+          src="../../../public/img-three.png"
+          className="w-full"
+        />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
