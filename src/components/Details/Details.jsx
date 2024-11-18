a8ef650b2772bb53a90a30aebe97726e066af83e
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-11-18 17:53:33 +0600

    feedback section done

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { useLoaderData } from "react-router-dom";
 
 const Details = () => {
   const {
-    id,
     image,
     serviceName,
     category,
@@ -14,12 +13,21 @@ const Details = () => {
     counselor,
     rating,
   } = useLoaderData();
+
+  const [feedback, setFeedback] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const handleFeedback = () => {
+    setFeedback(inputValue);
+    setInputValue(""); 
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen my-10">
       <div className="w-11/12 mx-auto hero-content flex-col lg:flex-row-reverse">
         <img
           src={image}
           className="max-w-lg w-full h-full rounded-lg shadow-2xl"
+          alt={serviceName}
         />
         <div>
           <h2 className="card-title text-3xl my-4">
@@ -39,7 +47,7 @@ const Details = () => {
           </h1>
           <p className="py-6">{description}</p>
           <div className="my-2 flex items-center gap-3">
-            Time :{duration}
+            Time: {duration}
             <p className="badge badge-accent">{category}</p>
           </div>
           <br />
@@ -50,13 +58,22 @@ const Details = () => {
                 <input
                   className="input input-bordered join-item"
                   placeholder="feedback"
+                  value={inputValue} 
+                  onChange={(e) => setInputValue(e.target.value)} 
                 />
               </div>
             </div>
             <div className="indicator">
-              <button className="btn btn-accent join-item">Feedback</button>
+              <button
+                onClick={handleFeedback}
+                className="btn btn-accent join-item"
+              >
+                Feedback
+              </button>
             </div>
           </div>
+         
+          <div className="font-bold my-10">Feedback: {feedback}</div>
         </div>
       </div>
     </div>
